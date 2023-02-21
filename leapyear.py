b0bdddac36f02a2b004d0373b2fa17ea710e7718
coenbg13 <124922135+coenbg13@users.noreply.github.com>
2023-02-21 09:33:28 -0800

    Add files via upload

---
+++ leapyear.py
@@ -0,0 +1,17 @@
+#playing with logic and trying something sort of complicated 
+#will determine if any year was a leap year
+year = int(input("Which year do you want to check? "))
+
+
+
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print('Leap year.')
+        else:
+            print('Not Leap Year.')
+    else:
+        print('Leap Year.')
+else:
+    print('Not leap year.')    
